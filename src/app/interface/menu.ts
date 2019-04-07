f5f4f773427e565e8f6363ad4509f5515095e26b
王品朝 <wangqihuan@viewhigh.com>
2019-04-08 07:32:13 +0800

    upload menu list

--- src/app/interface/menu.ts
+++ src/app/interface/menu.ts
@@ -12,7 +12,7 @@ export const Menu = {
     AngularBasic: [
         'heros',
         'communication',
-        'heros',
+        'directivesOrpip',
         'form',
     ],
     AngularRxjs: ['AngularRxjs'],
