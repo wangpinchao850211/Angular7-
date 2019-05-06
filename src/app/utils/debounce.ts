93ee7bcf6d0cc4926ebfad53a5a24367ce2ffb8f
Wang <qihuan.wang@accenture.com>
2019-05-06 19:10:06 +0800

    debounce原理

---
+++ src/app/utils/debounce.ts
@@ -0,0 +1,14 @@
+// 去抖函数原理
+export function debounce(fn, wait) {
+    console.log('debounce');
+    let timeout = null;
+
+    // tslint:disable-next-line:only-arrow-functions
+    return function() {
+        if (timeout !== null) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(fn, wait);
+        console.log('timeout', timeout);
+    };
+}
