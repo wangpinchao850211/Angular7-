64873cadc798a9b3d419baea561313656503fc1d
Wang <qihuan.wang@accenture.com>
2019-07-15 18:58:32 +0800

    rem base

--- src/app/rem/remlayout/remlayout.component.ts
+++ src/app/rem/remlayout/remlayout.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemlayoutComponent implements OnInit {
 
+  public tabs = [
+		{ id: 1, title: 'menu1', active: true },
+		{ id: 2, title: 'menu2', active: false },
+		{ id: 3, title: 'menu3', active: false },
+		{ id: 4, title: 'menu4', active: false },
+		{ id: 5, title: 'menu5', active: false },
+		{ id: 6, title: 'menu6', active: false },
+    { id: 7, title: 'menu7', active: false }
+  ];
+  contentWidth: string = `1000px`;
+  contentHeight: string = `500px`;
+  contentPadding: string = `20px`;
+  contentMargin: string = `30px`;
   constructor() { }
 
   ngOnInit() {
   }
 
+  menuclick(i) {
+    this.tabs.forEach((item) => {
+      item.active = false;
+    })
+    this.tabs.filter((s) => {
+      if(s.id === i.id) s.active = true;
+    })
+  }
 }
