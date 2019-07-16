ca7afda160012eaf59e4cbd04c52838eafda78f4
Wang <qihuan.wang@accenture.com>
2019-07-16 11:54:47 +0800

    rem 响应式 completed

--- src/app/rem/remlayout/remlayout.component.ts
+++ src/app/rem/remlayout/remlayout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-remlayout',
@@ -16,13 +16,50 @@ export class RemlayoutComponent implements OnInit {
 		{ id: 6, title: 'menu6', active: false },
     { id: 7, title: 'menu7', active: false }
   ];
-  contentWidth: string = `1000px`;
-  contentHeight: string = `500px`;
-  contentPadding: string = `20px`;
-  contentMargin: string = `30px`;
+  contentWidth: string;
+  contentHeight: string;
+  contentPaddingLeft: string;
+  contentPaddingRight: string;
+  contentPaddingTop: string;
+  contentPaddingBottom: string;
+  contentMarginLeft: string;
+  contentMarginRight: string;
+  contentMarginTop: string;
+  contentMarginBottom: string;
+  contentMargin: string;
+  @HostListener('window:resize')
+  onWindowResize() {
+     this.getStryle();
+  }
   constructor() { }
 
   ngOnInit() {
+    this.getStryle();
+  }
+
+  getStryle() {
+    const desc = <HTMLElement>document.getElementsByClassName('content-describe')[0];
+    // console.log(desc.offsetWidth);
+    // console.log(desc.offsetHeight);
+    // 直接desc.style.marginLeft 获取不到，要获取渲染完后的样式使用getComputedStyle
+    // console.log(window.getComputedStyle(desc,null).marginLeft);
+    // console.log(window.getComputedStyle(desc,null).marginTop);
+    // console.log(window.getComputedStyle(desc,null).marginRight);
+    // console.log(window.getComputedStyle(desc,null).marginBottom);
+    // console.log(window.getComputedStyle(desc,null).paddingLeft);
+    // console.log(window.getComputedStyle(desc,null).paddingTop);
+    // console.log(window.getComputedStyle(desc,null).paddingRight);
+    // console.log(window.getComputedStyle(desc,null).paddingBottom);
+    this.contentWidth = `${desc.offsetWidth}px`;
+    this.contentHeight = `${desc.offsetWidth}px`;
+    this.contentPaddingLeft = window.getComputedStyle(desc,null).paddingLeft;
+    this.contentPaddingRight = window.getComputedStyle(desc,null).paddingRight;
+    this.contentPaddingTop = window.getComputedStyle(desc,null).paddingTop;
+    this.contentPaddingBottom = window.getComputedStyle(desc,null).paddingBottom;
+    this.contentMarginLeft = window.getComputedStyle(desc,null).marginLeft;
+    this.contentMarginTop = window.getComputedStyle(desc,null).marginTop;
+    this.contentMarginRight = window.getComputedStyle(desc,null).marginRight;
+    this.contentMarginBottom = window.getComputedStyle(desc,null).marginBottom;
   }
 
   menuclick(i) {
