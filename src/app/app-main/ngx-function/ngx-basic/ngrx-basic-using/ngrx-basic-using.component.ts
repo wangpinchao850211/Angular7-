9ebd6b4e2a87fbd1fcea203a2692773983f7385c
Wang <qihuan.wang@accenture.com>
2019-07-19 19:00:01 +0800

    add rxjs operators

--- src/app/app-main/ngx-function/ngx-basic/ngrx-basic-using/ngrx-basic-using.component.ts
+++ src/app/app-main/ngx-function/ngx-basic/ngrx-basic-using/ngrx-basic-using.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, fromEvent, timer, Subject } from 'rxjs';
-import { switchMap, debounceTime, distinctUntilChanged, map, filter } from 'rxjs/operators';
+import { interval, fromEvent, timer, Subject, throwError, Subscriber, Subscription, BehaviorSubject, forkJoin, of, from } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, map, filter, catchError, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ngrx-basic-using',
@@ -9,6 +9,8 @@ import { switchMap, debounceTime, distinctUntilChanged, map, filter } from 'rxjs
 })
 export class NgrxBasicUsingComponent implements OnInit {
 
+  refreshNotif = new BehaviorSubject("");
+  subscription: Subscription;
   private searchTerms = new Subject<string>();
   public timerVal: number = 0;
 
@@ -57,9 +59,37 @@ export class NgrxBasicUsingComponent implements OnInit {
         debounceTime(300), // 流更新结束后 0.3s 才会通知观察者 
         distinctUntilChanged(), // 是说只有当值和上一次通知时的值不一样的时候才通知观察者 
         // switchMap(param => {console.log(param)})
+        catchError((err) => {        
+          // if (this.auth.IsTokenExpired(10)) {
+          //   this.auth.login();
+          // }
+          if(err.status == 500) {          
+            console.log(err.status);
+          }
+          return throwError(false);
+        }),
+        mergeMap((event: any) => {
+          // if (event instanceof HttpResponse && event.status === 200) {
+          //   return this.handleData(event)
+          // }
+          return of(event)
+        })
     ).subscribe(result => {
         console.log(result);
     });
+
+    // Ro Web Main
+    this.subscription = timer(300000, 300000).subscribe(() => {
+      // if (this.authenticationService.IsTokenExpired(20)) {
+      //   console.log("Renewing Token...")
+      //   this.RenewToken();
+      // }
+      // else {
+      //   console.log("Token Valid")
+      // }
+    });
   }
+
+  use
 
 }
