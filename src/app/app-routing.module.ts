3f18104cb8b08f323db47b2bcff5a927581aca73
Wang <qihuan.wang@accenture.com>
2019-08-07 17:04:02 +0800

    router log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -126,7 +126,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+      )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
