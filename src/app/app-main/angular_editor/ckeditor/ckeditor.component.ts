142d37463ddbef0861f035cb44ef84e154e49e3e
Wang <qihuan.wang@accenture.com>
2020-02-21 14:54:52 +0800

    使用js控制input与span的切换， 添加title可编辑

--- src/app/app-main/angular_editor/ckeditor/ckeditor.component.ts
+++ src/app/app-main/angular_editor/ckeditor/ckeditor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 // import * as ClassicEditor from '../ckeditor.js';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-ckeditor',
@@ -14,8 +14,11 @@ export class CkeditorComponent implements OnInit, OnDestroy {
   public Editor = ClassicEditor;
   public ckeditorInstance;
   public model = {
-    editorData: '<p>Hello, world!</p>'
+    editorData: '<p>Hello, world!</p>',
+    showEditer: false, // 标识是否显示隐藏
+    Width: 'auto'
   };
+  public sourceData: null;
   public isDisabled = false; // 控制编辑器只读状态
   config = {
       toolbar: [ 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote' ]
@@ -26,7 +29,7 @@ export class CkeditorComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-
+    this.sourceData = _.cloneDeep(this.model); // 存储一下数据，取消时可以恢复
   }
 
   public onReady( editor ) {
@@ -61,4 +64,29 @@ export class CkeditorComponent implements OnInit, OnDestroy {
     // } );
   }
 
+  GetContentWidth(value){ // 获取字符长度，可以对着一下与offsetWidth的区别
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "22px graphik-medium";
+    return ctx.measureText(value).width -20;
+  }
+  // 使用js控制span和input切换编辑输入
+  modifyContent(input, data) {
+    data.editorData = input.value;
+    data.Width = this.GetContentWidth(data.editorData);
+  }
+  TitleEdit(event, data) {
+    data.Width = this.GetContentWidth(data.editorData);
+    data.showEditer = !data.showEditer;
+    event.stopPropagation();
+  }
+  TitleSave(event, data) {
+    data.showEditer = !data.showEditer;
+    this.sourceData = _.cloneDeep(this.model); // 更新存储数据，取消时可以恢复
+    event.stopPropagation();
+  }
+  TitleCancel(event) {
+    this.model = _.cloneDeep(this.sourceData);
+    event.stopPropagation();
+  }
 }
